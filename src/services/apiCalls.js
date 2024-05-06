277733f3f8daf147f9548709cd6e5ad134569292
jluisferrer <jluisferrermartinez@gmail.com>
2024-05-06 19:53:39 +0200

    feat: adding deleteEvent to apicalls

--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -235,3 +235,24 @@ export const updateEvent = async (enventId, eventData, token) => {
         return error;
     }
 }
+
+export const deleteEvent = async (eventId, token) => {
+    const options = {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`,
+        },
+    };
+    try {
+        const response = await fetch(`${root}events/${eventId}`, options);
+        const data = await response.json();
+
+        if (!data.success) {
+            throw new Error(data.message);
+        }
+        return data;
+    } catch (error) {
+        return error;
+    }
+}
