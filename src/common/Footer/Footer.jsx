76be9d72224623db09d088fad8dbe0369d97cd2c
jluisferrer <jluisferrermartinez@gmail.com>
2024-05-11 13:43:39 +0200

    update:redesign header and footer styles

--- src/common/Footer/Footer.jsx
+++ src/common/Footer/Footer.jsx
@@ -5,7 +5,11 @@ export const Footer = (() => {
 
     return (
         <div className="footerDesign">
-            Soy footer
+            <div className="socialMediaIcons">
+                <span role="img" aria-label="Facebook">📘</span>
+                <span role="img" aria-label="Twitter">🐦</span>
+                <span role="img" aria-label="Instagram">📸</span>
+            </div>
         </div>
     )
 })
